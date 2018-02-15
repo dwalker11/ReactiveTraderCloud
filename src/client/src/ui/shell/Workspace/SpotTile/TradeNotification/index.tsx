0f8f86f6e06d9cf5263c80130769730683abc12e
Devon Walker <dwalker11@users.noreply.github.com>
2018-02-15 10:35:38 -0500

    Moved TradeNotification directory

---
+++ src/client/src/ui/shell/Workspace/SpotTile/TradeNotification/index.tsx
@@ -0,0 +1,65 @@
+import './styles/TradeNotificationStyles.scss'
+import * as classnames from 'classnames'
+import * as React from 'react'
+import { CurrencyPair, Notification, TradeStatus } from '../../../../../types'
+
+interface TradeNotificationProps {
+  currencyPair: CurrencyPair
+  notification: Notification
+  onDismissedClicked: () => void
+}
+
+class TradeNotification extends React.Component<TradeNotificationProps, {}> {
+  props: TradeNotificationProps
+
+  renderError() {
+    return (
+      <div className="trade-notification">
+        The execution status is unknown. Please contact your sales rep.
+        <div className="trade-notification__button-dismiss" onClick={this.props.onDismissedClicked}>
+          Done
+        </div>
+      </div>
+    )
+  }
+
+  render() {
+    const { notification, onDismissedClicked } = this.props
+
+    if (notification.hasError) {
+      return this.renderError()
+    }
+
+    const containerClassName = classnames('trade-notification', { 'trade-notification--rejected': notification.status === TradeStatus.Rejected })
+    return (
+      <div className={containerClassName}>
+        <span className="trade-notification__trade-status">{notification.status}</span>
+        <div className="trade-notification__summary-item--direction">{notification.direction}</div>
+        <div className="trade-notification__summary-item--notional">
+          {notification.dealtCurrency} {notification.notional}
+        </div>
+        <div className="trade-notification__summary-item--currency">
+          <span className="trade-notification__label--versus">vs </span>
+          {this.props.currencyPair.terms}
+        </div>
+        <div className="trade-notification__details-items-container">
+          {this.createItemDetailElement('Rate', notification.spotRate)}
+          {this.createItemDetailElement('Date', notification.formattedValueDate)}
+          {this.createItemDetailElement('Trade ID', notification.tradeId)}
+        </div>
+        <i className="trade-notification__dismiss-icon fa fa-share" onClick={onDismissedClicked} />
+      </div>
+    )
+  }
+
+  private createItemDetailElement = (label: string, value: string | number) => {
+    return (
+      <div className="trade-notification__details-item">
+        <div className="trade-notification__details-item--label">{label}</div>
+        <div className="trade-notification__details-item--value">{value}</div>
+      </div>
+    )
+  }
+}
+
+export default TradeNotification
