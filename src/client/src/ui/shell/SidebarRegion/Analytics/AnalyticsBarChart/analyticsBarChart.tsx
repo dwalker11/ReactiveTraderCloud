755af8469e0bb25f82625a5cc78db53f4a5640f6
Devon Walker <dwalker11@users.noreply.github.com>
2018-02-15 12:57:37 -0500

    Moved AnalyticsBarChart directory

---
+++ src/client/src/ui/shell/SidebarRegion/Analytics/AnalyticsBarChart/analyticsBarChart.tsx
@@ -0,0 +1,32 @@
+import * as _ from 'lodash'
+import * as React from 'react'
+import PNLBar from './PNLBar'
+import { CurrencyPair } from '../../../../../types'
+
+export interface AnalyticsBarChartProps {
+  chartData: any[]
+  currencyPairs: CurrencyPair[]
+  isPnL: boolean
+}
+
+class AnalyticsBarChart extends React.Component<AnalyticsBarChartProps, {}> {
+  render() {
+    return <div>{!_.isEmpty(this.props.chartData) && this.createBars()}</div>
+  }
+
+  createBars() {
+    const { isPnL, chartData } = this.props
+    const baseValues: any[] = _.map(chartData, 'basePnl')
+    const maxValue: number = _.max(baseValues)
+    const minValue: number = _.min(baseValues)
+    const maxWidth = Math.max(Math.abs(maxValue), Math.abs(minValue))
+    const bars = chartData.map((chartItem, index) => {
+      const { basePnl, symbol } = chartItem
+      const currencyPair = this.props.currencyPairs[symbol]
+      return <PNLBar key={index} index={index} basePnl={basePnl} symbol={symbol} currencyPair={currencyPair} isPnL={isPnL} maxVal={maxWidth} />
+    })
+    return bars
+  }
+}
+
+export default AnalyticsBarChart
