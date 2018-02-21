e962b8e54e920c718ec055ef8c0532b94a714d53
Devon Walker <dwalker11@users.noreply.github.com>
2018-02-21 09:20:56 -0500

    Cleaned up Blotter type dependices

--- src/client/src/ui/blotter/index.ts
+++ src/client/src/ui/blotter/index.ts
@@ -3,3 +3,5 @@ export { default as Blotter } from './blotter'
 export { default as DateCell } from './DateCell'
 export { default as blotterEpic } from './epics'
 export { default as blotterReducer } from './reducer'
+
+export { CurrencyPair, Trade, TradeStatus } from '../../types'
