ba9acdf02c522f3235ef4018dc43ef461ece5f81
Devon Walker <dwalker11@users.noreply.github.com>
2018-03-28 21:57:29 -0400

    Render Modal component using emotion

--- src/client/src/ui/modal/Modal.tsx
+++ src/client/src/ui/modal/Modal.tsx
@@ -1,6 +1,45 @@
 import * as React from 'react'
+import styled from 'react-emotion'
 
 import './ModalStyles.scss'
+
+const ModalOverlay = styled('div')`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  opacity: .7;
+  z-index: 100;
+  background: rgba(0, 0, 0, 0.8);
+`
+
+const Modal = styled('div')`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 600px;
+  height: 150px;
+  margin: auto;
+  background-color: white;
+  text-align: center;
+  z-index: 101;
+`
+
+const ModalHeader = styled('div')`
+  border-bottom: 1px solid #da4d2e;
+`
+
+const ModalHeaderTitle = styled('span')`
+  display: inline-block;
+  padding: 5px;
+`
+
+const ModalBody = styled('div')`
+  padding: 5px;
+`
 
 export interface ModalProps {
   shouldShow?: boolean
@@ -10,23 +49,24 @@ export interface ModalProps {
 
 // TODO disable tabbing outside of the modal
 // tslint:disable-next-line:variable-name
-const Modal = (props: ModalProps) => {
+export default function(props: ModalProps) {
   if (!props.shouldShow) {
     return null
   }
+
   return (
     <div>
-      <div className="modal__overlay"></div>
-      <div className="modal">
-        <div className="modal__header">
-          <span className="modal__header-title">{props.title}</span>
-        </div>
-        <div className="modal__body">
+      <ModalOverlay/>
+      <Modal>
+        <ModalHeader>
+          <ModalHeaderTitle>
+            {props.title}
+          </ModalHeaderTitle>
+        </ModalHeader>
+        <ModalBody>
           {props.children}
-        </div>
-      </div>
+        </ModalBody>
+      </Modal>
     </div>
   )
 }
-
-export default Modal
