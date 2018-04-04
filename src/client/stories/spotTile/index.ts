38e4dc5f5ace4a3038664c4b676e6a8e5b80ccdf
Devon Walker <dwalker11@users.noreply.github.com>
2018-04-04 14:30:28 -0400

    Added missing a type property in SpotTileProps (storybook)

--- src/client/stories/spotTile/index.ts
+++ src/client/stories/spotTile/index.ts
@@ -1,55 +1,56 @@
-import { Direction } from '../../src/types/direction'
-import { NotificationType } from '../../src/types/notificationType'
-import { SpotTileProps } from '../../src/ui/spotTile/SpotTile'
-import { Trade } from '../../src/types/trade'
+import { Direction, NotificationType, Trade } from '../../src/types'
 import { Notification } from '../../src/types/notification'
+import { SpotTileProps } from '../../src/ui/spotTile/SpotTile'
 
-const getRandomNumber = (min: number, max: number) =>
-  Math.floor(Math.random() * (max - min + 1) + min)
+const getRandomNumber = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1) + min)
 
 export const getContainerStyling = {
   margin: '50px auto 0',
   width: '30%',
   minWidth: '300px',
-  padding: '5px',
+  padding: '5px'
 }
 
-export const getSpotTileProps: () => SpotTileProps = (executionConnected: boolean = true, isTradeExecutionInFlight: boolean = false) => {
+export const getSpotTileProps: () => SpotTileProps = (
+  executionConnected: boolean = true,
+  isTradeExecutionInFlight: boolean = false
+) => {
   return {
     executionConnected,
     isTradeExecutionInFlight,
-    spotTileData: null,
     canPopout: true,
     currencyChartIsOpening: false,
-    currencyPair: {
-      symbol: 'GBP',
-      base: 'GBP',
-      ratePrecision: 0,
-      pipsPosition: 0,
-      terms: '',
-    },
     currentSpotPrice: {
       ask: getButtonProps('ask', () => {}).rate,
       bid: getButtonProps('ask', () => {}).rate,
       priceMovementType: 'Up',
       spread: {
-        formattedValue: '-1.23',
+        formattedValue: '-1.23'
       },
-      valueDate: 1234436547,
+      valueDate: 1234436547
     },
     hasNotification: false,
-    isRunningInOpenFin: false,
     maxNotional: 5000000,
     notification: {
       error: null,
       notificationType: NotificationType.Trade,
-      hasError: false,
+      hasError: false
     },
     notional: 500,
     priceStale: false,
     title: 'GBP / USD',
-    executeTrade: () => {},
+    currencyPair: {
+      symbol: 'GBP',
+      base: 'GBP',
+      ratePrecision: 0,
+      pipsPosition: 0,
+      terms: ''
+    },
+    spotTileData: null,
+    pricingConnected: false,
+    isRunningInOpenFin: false,
     onComponentMount: () => {},
+    executeTrade: () => {},
     onPopoutClick: () => {},
     undockTile: () => {},
     displayCurrencyChart: () => {},
@@ -58,7 +59,6 @@ export const getSpotTileProps: () => SpotTileProps = (executionConnected: boolea
 }
 
 export const getButtonProps = (type: string, action: any) => {
-
   const classNameType = type === 'Sell' ? 'bid' : 'ask'
 
   return {
@@ -68,9 +68,9 @@ export const getButtonProps = (type: string, action: any) => {
       pips: getRandomNumber(0, 99),
       bigFigure: getRandomNumber(0, 150),
       pipFraction: getRandomNumber(0, 9),
-      rawRate: 123,
+      rawRate: 123
     },
-    onExecute: action('buy clicked'),
+    onExecute: action('buy clicked')
   }
 }
 
@@ -80,20 +80,19 @@ export const getNotionalInputProps = {
   currencyPair: { symbol: 'GBP', base: 'GBP', ratePrecision: 0, pipsPosition: 0, terms: '' },
   onChange: () => console.log('Changed'),
   maxValue: 5000000,
-  onNotionalInputChange: () => {},
+  onNotionalInputChange: () => {}
 }
 
-export const getPriceMovementIndicatorProps = (priceMovementType: string,
-                                               formattedValue: string) => {
+export const getPriceMovementIndicatorProps = (priceMovementType: string, formattedValue: string) => {
   return {
     priceMovementType,
     spread: {
-      formattedValue,
-    },
+      formattedValue
+    }
   }
 }
 
-export const getRandomTrade = (direction:string, status:string):Trade => {
+export const getRandomTrade = (direction: string, status: string): Trade => {
   return {
     status,
     direction,
@@ -105,13 +104,11 @@ export const getRandomTrade = (direction:string, status:string):Trade => {
     termsCurrency: 'USD',
     spotRate: 123,
     tradeDate: new Date(),
-    valueDate: new Date(),
+    valueDate: new Date()
   }
 }
 
-export const getTradeNotification = (status: string,
-                                     direction: string,
-                                     hasError: boolean):Notification => {
+export const getTradeNotification = (status: string, direction: string, hasError: boolean): Notification => {
   return {
     hasError,
     notificationType: NotificationType.Trade,
